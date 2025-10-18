e32f3629a8e6e5235c1800f44e6e90de54cb4e49
brossjonk-pixel <anastantan23@gmail.com>
2025-10-18 18:16:47 +0100

    first-commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/transportation-app/'  // <<< required for GitHub Pages
 })

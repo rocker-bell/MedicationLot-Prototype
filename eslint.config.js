34903352c75f4641355de358328b8cd27e5972fe
Anass Tantane <rockbell8@gmail.com>
2025-11-24 21:34:59 +0100

    use of eslint - test compability fix

--- eslint.config.js
+++ eslint.config.js
@@ -1,29 +1,65 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import { defineConfig, globalIgnores } from 'eslint/config'
+// import js from '@eslint/js'
+// import globals from 'globals'
+// import reactHooks from 'eslint-plugin-react-hooks'
+// import reactRefresh from 'eslint-plugin-react-refresh'
+// import { defineConfig, globalIgnores } from 'eslint/config'
 
-export default defineConfig([
-  globalIgnores(['dist']),
-  {
-    files: ['**/*.{js,jsx}'],
-    extends: [
-      js.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
-    ],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-      parserOptions: {
-        ecmaVersion: 'latest',
-        ecmaFeatures: { jsx: true },
-        sourceType: 'module',
-      },
-    },
-    rules: {
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+// export default defineConfig([
+//   globalIgnores(['dist']),
+//   {
+//     files: ['**/*.{js,jsx}'],
+//     extends: [
+//       js.configs.recommended,
+//       reactHooks.configs.flat.recommended,
+//       reactRefresh.configs.vite,
+//     ],
+//     languageOptions: {
+//       ecmaVersion: 2020,
+//       globals: globals.browser,
+//       parserOptions: {
+//         ecmaVersion: 'latest',
+//         ecmaFeatures: { jsx: true },
+//         sourceType: 'module',
+//       },
+//     },
+//     rules: {
+//       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+//     },
+//   },
+// ])
+
+
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    jest: true,
+  },
+  parser: '@babel/eslint-parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
     },
   },
-])
+  extends: [
+    'airbnb',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+  ],
+  plugins: ['react'],
+  rules: {
+    'react/jsx-filename-extension': ['warn', { extensions: ['.js', '.jsx'] }],
+    'react/react-in-jsx-scope': 'off',
+    'import/no-unresolved': 'off',
+    'no-shadow': 'off',
+  },
+  ignorePatterns: ['dist/', 'build/'],
+  overrides: [
+    {
+      files: ['src/**/*Slice.js'],
+      rules: { 'no-param-reassign': ['error', { props: false }] },
+    },
+  ],
+};
